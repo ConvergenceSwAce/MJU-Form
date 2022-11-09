e1dca1ea8d3b0722fa6959bb9e0327a8b497a06c
MinsuKim <minsu020302@gmail.com>
2022-11-10 00:57:47 +0900

    Feat : 컴포넌트 완성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Survey from "./pages/Survey";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Survey from './pages/Survey';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          {/* <Route path="/" element={<Main />} /> */}
+          <Route path="/" element={<Main />} />
           <Route path="/survey" element={<Survey />} />
         </Routes>
       </Router>
---
+++ src/components/Header.js
@@ -0,0 +1,140 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+
+const Header = () => {
+  //  Menu Opne, close state
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar color="header" position="relative" elevation={0}>
+      <Container maxWidth="x">
+        <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
+          {/* 로고 */}
+          <IconButton href="/">
+            <Box
+              component="img"
+              sx={{
+                width: 50,
+              }}
+              src="/image/1.gif"
+            />
+            <Typography
+              variant="h5"
+              style={{ color: '#000', marginLeft: '1vw' }}
+            >
+              test
+            </Typography>
+          </IconButton>
+
+          {/* 메뉴 버튼 for mobile */}
+          <Box
+            sx={{
+              display: {
+                xs: 'flex',
+                sm: 'none',
+                justifyContent: 'flex-end',
+              },
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="icon"
+            >
+              {/* <MenuIcon /> */}
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', sm: 'none' },
+              }}
+            >
+              <a
+                href="/check"
+                style={{
+                  textDecoration: 'none',
+                  color: 'inherit',
+                }}
+              >
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">내 예약 확인</Typography>
+                </MenuItem>
+              </a>
+              <a
+                href="/contact"
+                style={{
+                  textDecoration: 'none',
+                  color: 'inherit',
+                }}
+              >
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">문의사항</Typography>
+                </MenuItem>
+              </a>
+            </Menu>
+          </Box>
+
+          {/* 메뉴 버튼 for desktop */}
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: {
+                xs: 'none',
+                sm: 'flex',
+                justifyContent: 'flex-end',
+              },
+            }}
+          >
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, mr: 1, color: 'white', display: 'block' }}
+              href="/check"
+            >
+              <Typography variant="h7">내 예약 확인</Typography>
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, mr: 1, color: 'white', display: 'block' }}
+              href="/contact"
+            >
+              <Typography variant="h7">문의사항</Typography>
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default Header;
